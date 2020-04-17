37157a84381fd921d80b5dc40524ec2e907f31ab
skylarkgit <abhay199658@gmail.com>
2020-04-17 21:59:38 +0530

    [upd] filter is optional

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ import time
 def timeFilter(path, min_time):
     return os.path.getmtime(path) >= min_time
 
-def getFileList(path, filter):
+def getFileList(path, filter = None):
     fArr = []
     for r, d, f in os.walk(path):
         for i in f:
-            if filter(os.path.join(r,i)):
-                fArr.append(os.path.join(r,i))
+            if filter is not None: 
+                if filter(os.path.join(r,i)):
+                    fArr.append([r, i])
+            else:
+                fArr.append([r, i])
     return fArr
 
 for f in getFileList('/home/abhay-verma/temp/tcs/sample-root', lambda path: timeFilter(path, 1587139722)):
-    print(f, os.path.getmtime(f))
+    print('file "' + f[1] + '" in "' + f[0] + '" has changed recently')
