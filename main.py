5d0e9d0488143397b28928fa46ac27ff72ed33a3
skylarkgit <abhay199658@gmail.com>
2020-04-17 22:05:40 +0530

    [add] command line arguments

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import time
+import sys
 
 def timeFilter(path, min_time):
     return os.path.getmtime(path) >= min_time
@@ -14,6 +15,11 @@ def getFileList(path, filter = None):
             else:
                 fArr.append([r, i])
     return fArr
-
-for f in getFileList('/home/abhay-verma/temp/tcs/sample-root', lambda path: timeFilter(path, 1587139722)):
-    print('file "' + f[1] + '" in "' + f[0] + '" has changed recently')
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print('correct usage: python main.py <path> <min_epoch_time_in_sec>')
+    else:
+        _path = sys.argv[1]
+        _min_time = float(sys.argv[2])
+        for f in getFileList(_path, lambda path: timeFilter(path, _min_time)):
+            print('file "' + f[1] + '" in "' + f[0] + '" has changed recently')
